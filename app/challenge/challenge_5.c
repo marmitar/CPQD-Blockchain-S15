6b284930ff26226b18a68ecd1ae9186536cb0b32
Tiago de Paula <tiagodepalves@gmail.com>
2025-07-03 02:20:38 -0300

    feat: stocastical challenge 5

--- app/challenge/challenge_5.c
+++ app/challenge/challenge_5.c
@@ -1,5 +1,8 @@
+#define _GNU_SOURCE 1  // required for M_PI
+
 #include <inttypes.h>
 #include <limits.h>
+#include <math.h>
 #include <sgx_eid.h>
 #include <sgx_error.h>
 #include <stddef.h>
@@ -58,174 +61,176 @@ static uint8_t check_answers(const sgx_enclave_id_t eid, sgx_status_t *NONNULL s
     return likely(wins != ROUNDS) ? (uint8_t) wins : UINT8_MAX;
 }
 
+[[gnu::const, nodiscard("pure function")]]
 /**
- * How to initialize each position of the `answers` array. See `init_greedy`.
- */
-enum [[gnu::packed]] init_mode_t {
-    /** Set all values to zero. */
-    INIT_ZERO,
-    /** Set all values to `position + start`. */
-    INIT_ADD,
-    /** Set all values to `position * start`, with `+ 1` for more mixing. */
-    INIT_MUL,
-    /** Set all values to `position**2 + start**2`. */
-    INIT_SQUARE,
-};
-
-[[gnu::const, nodiscard("pure function"), gnu::hot]]
-/**
- * Function to initialize the `answers` array (or part of it). It receives the current `position` being initialized
- * and the the `start` position for the current initialization process, and returns `0` (rock), `1` (paper), or
- * `2` (scissors) to be used as `answers` for that `position`, according to the `init` mode given.
+ * Sergei Winitzki's approximation to inverse error function ($erf^{-1}$). Used as starting point for `erfinv`.
  *
- * This function is used in the `greedy` hill climbing solution as a starting point, and is extremely important for
- * finding a good local maximum of high wins.
+ * @see https://www.scribd.com/document/82414963/Winitzki-Approximation-to-Error-Function
  */
-static uint8_t init_greedy(size_t position, size_t start, enum init_mode_t init) {
-    assume(position <= ROUNDS);
-    assume(start <= ROUNDS);
+static double erfinv_approx(const double x) {
+    assume(isgreater(x, -1) && isless(x, 1));
 
-    switch (init) {
-        case INIT_ZERO:
-            return 0;
-        case INIT_ADD:
-            return (uint8_t) (position + start) % 3U;
-        case INIT_MUL:
-            return (uint8_t) (position * start + 1) % 3U;
-        case INIT_SQUARE:
-            return (uint8_t) (position * position + start * start) % 3U;
-    }
+    const double xa = fabs(x);
+    const double lxa = log(1 - xa * xa);
 
-    printf("Challenge 5: Invalid enum init_mode_t = %d\n", (int) init);
-    return UINT8_MAX;
+    static const double a = (8 * (M_PI - 3)) / (3 * M_PI * (4 - M_PI));
+
+    const double v = 2 / (M_PI * a) + (1.0 / 2) * lxa;
+    const double y = -v + sqrt(v * v - (1.0 / a) * lxa);
+
+    return copysign(y, x);
 }
 
-[[nodiscard("error must be checked"), gnu::nonnull(2), gnu::hot]]
+[[gnu::const, nodiscard("pure function")]]
 /**
- * (Partial) Greedy Hill Climbing solution
- * ---------------------------------------
+ * Newton-Raphson's method for finding the inverse error function ($erf^{-1}$).
  *
- * This algorithm tries to find the solution for the Rock, Paper, Scissors by only testing one position at a time.
- * For each position, it tests the values `0` (rock), `1` (paper) and `2` (scissors), then sets on the one with
- * highest wins. This solution can be applied partially to subrange of the array, but choosing the correct `start`
- * index. The `init` function is used to set the starting point for hill climbing, see `init_function_t`. The greedy
- * seach starts from the end of the array.
- *
- * This implementation is considerably faster, but it's very unlikely to reach the global maximum (i.e. the actual
- * solution) by itself, but can be used to approximate how correct the unmodified positions (from `0` to `start`) are.
- * The `ecall_pedra_papel_tesoura` is called exatcly `2 * (ROUNDS - start) + 1` times. For example, at `start = 0`,
- * `ecall_pedra_papel_tesoura` will be called 41 times.
- *
- * Returns the total number of wins for all checked `answers`, or `UINT32_MAX` if a solution was found. In the case of
- * errors, `UINT32_MAX` is also returned to stop the solution and an error code is written to `status`.
+ * @see https://en.wikipedia.org/wiki/Newton%27s_method
  */
-static uint32_t greedy(
-    const sgx_enclave_id_t eid,
-    sgx_status_t *NONNULL status,
-    const size_t start,
-    const enum init_mode_t init
+static double erfinv(const double x) {
+    if unlikely (islessequal(x, -1)) {
+        return -INFINITY;
+    } else if unlikely (isgreaterequal(x, 1)) {
+        return +INFINITY;
+    }
+
+    static const size_t ITERATIONS = 6;
+    const double M_SQRTPI = sqrt(M_PI);
+
+    double y = erfinv_approx(x);
+    for (size_t i = 0; i < ITERATIONS; i++) {
+        const double f = erf(y) - x;
+        const double fp = (2 / M_SQRTPI) * exp(-y * y);
+        y -= f / fp;
+    }
+    return y;
+}
+
+[[gnu::const, nodiscard("pure function")]]
+/**
+ * P-Quantile function of the Standard Normal Distribution.
+ *
+ * @see https://en.wikipedia.org/wiki/Quantile_function
+ */
+static double z(const double p) {
+    return sqrt(2) * erfinv(2 * p - 1);
+}
+
+[[gnu::const, nodiscard("pure function")]]
+/**
+ * Calculate `x**2`.
+ */
+static double square(double x) {
+    return x * x;
+}
+
+[[gnu::const, nodiscard("pure function")]]
+/**
+ * Estimate sample size required for the given `confidence` and `power` when choosing the correct value for a position.
+ * This is based on classical inference using two-sided tests.
+ */
+static size_t sample_size(
+    /**
+     * 1 - α, or the likelihood that a Type-I error does not occur.
+     */
+    const double confidence,  // NOLINT(bugprone-easily-swappable-parameters)
+    /**
+     * 1 - β, or the likelihood that a Type-II error does not occur.
+     */
+    const double power,  // NOLINT(bugprone-easily-swappable-parameters)
+    /**
+     * (Assumed) Standard deviation of the sample.
+     */
+    const double sigma,
+    /**
+     * Expected gap between the correct choice and competitors.
+     */
+    const double delta
 ) {
-    assume(start <= 2ULL * ROUNDS);
-    // we must initialize the positions, so previous values don't interfere with the results
+    // Bonferroni-safe significance
+    const double alpha = (1 - confidence) / 2;
+    const double z1a = z(1 - alpha);
+    const double z1b = z(power);
+
+    const double n = (2 * square(z1a + z1b) * square(sigma)) / square(delta);
+    return (size_t) ceil(n);
+}
+
+[[gnu::const, nodiscard("pure function")]]
+/**
+ * Estimate standard deviation for the games assuming binomial distribution for the sequence.
+ */
+static double sigma(const size_t n) {
+    /** Winning probability for each position. */
+    const double p = 1.0 / 3.0;
+
+    return sqrt((double) n * (p * (1.0 - p)));
+}
+
+[[nodiscard("useless call otherwise")]]
+/**
+ * Generate an evenly distributed guess between `0` (rock), `1` (paper), or `2` (scissors).
+ */
+static uint8_t random_guess(void) {
+    static const int THRESHOLD = RAND_MAX - RAND_MAX % 3;
+
+    while (true) {
+        const int value = rand();  // NOLINT(cert-msc30-c, concurrency-mt-unsafe)
+        assume(value >= 0);
+
+        if likely (value < THRESHOLD) {
+            return (uint8_t) (value % 3);
+        }
+    }
+}
+
+[[gnu::hot]]
+/**
+ * Populate the last `ROUNDS - start` positions in `answers` with random guesses.
+ */
+static void generate_random_answers_from(const size_t start) {
     for (size_t i = start; i < ROUNDS; i++) {
-        answers[i] = init_greedy(i, start, init);
+        answers[i] = random_guess();
     }
-
-    // number of wins when: answers[i] = (init(i, start) + 0) % 3
-    uint8_t wins0 = check_answers(eid, status);
-    if unlikely (wins0 == UINT8_MAX) {
-        return UINT32_MAX;
-    }
-
-    uint32_t total_wins = wins0;
-    // we start from the end, so there's less interference of the pseudo-random Rock, Paper, Scissors sequence
-    for (size_t i = ROUNDS; i > start; i--) {
-        const uint8_t vi = answers[i - 1];
-
-        // number of wins when: answers[i] = (init(i, start) + 1) % 3
-        answers[i - 1] = (uint8_t) (vi + 1U) % 3U;
-        const uint8_t wins1 = check_answers(eid, status);
-        if unlikely (wins1 == UINT8_MAX) {
-            return UINT32_MAX;
-        }
-
-        // number of wins when: answers[i] = (init(i, start) + 2) % 3
-        answers[i - 1] = (uint8_t) (vi + 2U) % 3U;
-        const uint8_t wins2 = check_answers(eid, status);
-        if unlikely (wins2 == UINT8_MAX) {
-            return UINT32_MAX;
-        }
-
-        // select the highest one, and save to `wins0`
-        if (wins0 >= wins1 && wins0 >= wins2) {
-            answers[i - 1] = (uint8_t) (vi + 0U) % 3U;
-            // SKIP: wins0 = wins0;
-        } else if (wins1 > wins2) {
-            answers[i - 1] = (uint8_t) (vi + 1U) % 3U;
-            wins0 = wins1;
-        } else {
-            // SKIP: answers[i-1] = (uint8_t) (vi + 2U) % 3U;
-            wins0 = wins2;
-        }
-
-        // account all checked results to the total wins
-        total_wins += wins1;
-        total_wins += wins2;
-    }
-
-    return total_wins;
 }
 
 [[nodiscard("error must be checked"), gnu::nonnull(2), gnu::hot]]
 /**
- * Limited Depth-First Search solution
- * -----------------------------------
+ * Estimate the correct play for position `start` with 95% confidence.
  *
- * This implementation tries to apply the DFS to find the correct `answers`. To avoid an exponential bloww up, the
- * search is limited to `depth` recursive calls, so `depth` positions set by DFS. The remaining positions are searched
- * using `greedy` hill climbing from different positions.
+ * For each of the three possible values, `0` (rock), `1` (paper), or `2` (scissors), this function generates `n`
+ * random sub-sequences after `start` and selects the value with most wins in total. The correct value is expected to
+ * produce 1 more win on average than the other two possibilities, resulting in an expected `n` more wins on the
+ * aggregate.
  *
- * The idea here is that `limited_dfs` selects a good value `answers[start]` by changing its value between `0` (rock),
- * `1` (paper) and `2` (scissors) and checking multiple distinct configurations for the other positions. The correct
- * value should, on average, result in higher wins.
- *
- * This implementation is possibly exponential, but should be able to reach the global maximum (i.e. the actual
- * solution) if unbounded. Nevertheless, the bounded implementation can abuse statistics to get a very good guess for
- * each position (usually the correct one). This function calls `ecall_pedra_papel_tesoura` a total of
- * `3**depth * 4 * (2 * (ROUNDS - (start + depth)) + 1)` times. For example, at `start = 0` and `depth = 2`, the
- * `ecall_pedra_papel_tesoura` will be called 1332 times.
+ * The sample size `n` is estimated following a two-sided test of `ROUNDS - start` guesses with 1/3 win probability.
+ * This value is at most `n = 94`, for `start = 0` and 5% significance value. In total, `3 * n` calls to
+ * `ecall_pedra_papel_tesoura` are made.
  *
  * Returns the total number of wins for all checked `answers`, or `UINT32_MAX` if a solution was found. In the case of
- * errors, `UINT32_MAX` is also returned to stop the solution and an error code is written to `status`.
+ * errors, `UINT32_MAX` is also returned to stop the solution and an error code is written to `status`
  */
-static uint32_t limited_dfs(
-    const sgx_enclave_id_t eid,
-    sgx_status_t *NONNULL status,
-    const size_t start,
-    const size_t max_depth
-) {
-    if likely (max_depth == 0) {
-        const enum init_mode_t init[] = {INIT_ZERO, INIT_MUL, INIT_ADD, INIT_SQUARE};
+static uint32_t pick_position(const sgx_enclave_id_t eid, sgx_status_t *NONNULL status, const size_t start) {
+    /** 5% chance of assuming a value is better when all are equal. */
+    static const double CONFIDENCE = 0.95;
+    /** 10% chance of not picking the best value when there's one. */
+    static const double POWER = 0.90;
+    /** Correct choice always scores, and wrong ones never does. So 1 score higher is expected. */
+    static const double DELTA = 1;
 
-        uint32_t total_wins = 0;
-        for (size_t i = 0; i < sizeof(init) / sizeof(init[0]); i++) {
-            const uint32_t wins = greedy(eid, status, start, init[i]);
-            if unlikely (wins == UINT32_MAX) {
+    const size_t n = sample_size(CONFIDENCE, POWER, sigma(ROUNDS - start), DELTA);
+
+    uint32_t wins[3] = {0, 0, 0};
+    for (uint8_t d = 0; d < 3; d++) {
+        answers[start] = d;
+        for (size_t k = 0; k < n; k++) {
+            generate_random_answers_from(start + 1);
+
+            const uint8_t current_wins = check_answers(eid, status);
+            if unlikely (current_wins == UINT8_MAX) {
                 return UINT32_MAX;
             }
-            total_wins += wins;
-        }
-        return total_wins;
-    } else if (start >= ROUNDS) {
-        return 0;
-    }
-
-    uint32_t wins[3] = {UINT32_MAX, UINT32_MAX, UINT32_MAX};
-    for (uint8_t d = 0; d <= 2; d++) {
-        answers[start] = d;
-        wins[d] = limited_dfs(eid, status, start + 1, max_depth - 1);
-        if unlikely (wins[d] == UINT32_MAX) {
-            return UINT32_MAX;
+            wins[d] += current_wins;
         }
     }
 
@@ -247,20 +252,24 @@ static uint32_t limited_dfs(
  * Search for a winning rock, paper, scissors sequence using a mix of Depth Limited Search and Hill Climbing. For each
  * select position, all three values are tested in multiple different configurations, and the one with highest total
  * wins is selected. This is likely to be the correct result, because each correct position will yield more wins then
- * the other two on average, assuming the remaining rounds are indistinguishable from random (i.e. it's a PRGN). In
- * total, up to 13_032 calls to `ecall_pedra_papel_tesoura` are made.
+ * the other two on average, assuming the remaining rounds are indistinguishable from random (i.e. it's a PRNG). In
+ * total, up to 2979 calls to `ecall_pedra_papel_tesoura` are made.
  */
 extern sgx_status_t challenge_5(sgx_enclave_id_t eid) {
+    /** Randomly generated fixed seed (`openssl rand -hex 4`). */
+    static const unsigned SEED = 0x23'20'c5'da;
+    srand(SEED);  // NOLINT(cert-msc32-c)
+
     for (size_t start = 0; start < ROUNDS; start++) {
         sgx_status_t status = SGX_SUCCESS;
 
-        const size_t total_wins = limited_dfs(eid, &status, start, /*max_depth=*/2);
+        const uint32_t total_wins = pick_position(eid, &status, start);
         if likely (total_wins == UINT32_MAX) {
             return status;
         }
 
 #ifdef DEBUG
-        printf("Challenge 5: answers[%zu] = %" PRIu8 "\n", start, answers[start]);
+        printf("Challenge 5: answers[%zu] = %" PRIu8 ", total wins = %" PRIu32 "\n", start, answers[start], total_wins);
 #endif
     }
 
